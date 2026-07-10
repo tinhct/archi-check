c0b6e4787a669898514da9e8ca59ee1bafc0966f
Tinh Chau <chauthanhtinh@gmail.com>
2026-07-10 13:47:52 +0700

    feat(build): automate build-phase environment validation bypass using next phase constants

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import { PHASE_PRODUCTION_BUILD } from "next/constants";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = (phase: string): NextConfig => {
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    process.env.SKIP_ENV_VALIDATION = 'true';
+  }
+
+  return {
+    /* config options here */
+  };
 };
 
 export default nextConfig;
